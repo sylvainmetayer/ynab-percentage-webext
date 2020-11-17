ec66165bcf02dd747d68f61a72f455ef4acedb60
Sylvain <s.metayer@groupeonepoint.com>
2020-11-17 14:44:16 +0100

    feat: do not disconnect observer
    use fixed format for now

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # ynab-percentage-webext
-Basic WebExtension to add percentage on category group inside YNAB budget view
+
+Basic WebExtension to add percentage on category group inside [YNAB](https://youneedabudget.com/) budget view
+
+## Installation
+
+At the moment, the extension will run only run with some requirements :
+
+- You need to set your Currency number format to `123 456.789` in your YNAB settings.
+
+## Usage
+
+- `npm start` will run the extension locally in a temporary Firefox profile.
+- `npm start:chrome` will run the extension locally in a temporary Chrome profile.
+
+## Build
+
+- `npm run build`
+
+## Lint
+
+- `npm run lint`
+
+## Release
+
+- Create a tag, release will be created by the [Github Action](.github/workflows/release.yml)
+
+## TODO
+
+- [ ] Find a way to get errors displayed instead of being ignored ([link](https://extensionworkshop.com/documentation/develop/debugging/#debugging-content-scripts))
+- [ ] Support multiple number format and configure them
+- [ ] Package extension in repository and/or in [AMO](https://addons.mozilla.org/)
+- [ ] Chrome extension submission ?
--- src/index.js
+++ src/index.js
@@ -1,61 +1,87 @@
-const prefix = '[YNAB Percentage web extension]';
 const mainCategorySelector = '.is-master-category .budget-table-cell-budgeted .user-data.currency';
 
-const getNumberValue = function (value) {
-    const numberFormat = window.ynab.YNABSharedLib.currencyFormatter.getCurrency().example_format;
-    console.debug(`${prefix} Selected number format is '${numberFormat}'`);
+const log = {
+    _prefix: '[YNAB Percentage web extension]',
+    debug: (args) => {
+        console.debug(`${log._prefix} ${args}`);
+    },
+    log: (args) => {
+        console.log(`${log._prefix} ${args}`)
+    },
+    error: (args) => {
+        console.log('lol');
+        console.error(`${log._prefix} ${args}`)
+    },
+    warn: (args) => {
+        console.warn(`${log._prefix} ${args}`)
+    },
+}
+
+/**
+ * TODO Pass numberFormat and handle specials format
+ * @param {string} value
+ */
+const getNumberValue = (value) => {
+    const numberFormat = '123 456.789';
 
     if (numberFormat === '123 456.789') {
-        console.log({ number: value, numberFormat });
         return Number(value.replace(/[^0-9,\.]/g, ''));
     }
 
-    return null;
-
+    log.warn(`Number format '${numberFormat}' is not supported yet.`);
+    return 0;
     // TODO Handle others formats
 }
 
-const updateCategoriesPercentage = function (nodeCategories) {
+const updateCategoriesPercentage = (nodeCategories) => {
     const categories = Array.prototype.slice.call(nodeCategories);
     let total = 0;
     categories.forEach(mainCategory => {
-        const value = getNumberValue(mainCategory.textContent, numberFormat);
-        console.log(value);
-        total += value;
+        const value = mainCategory.textContent;
+        if (!value.match(/^.*%\).*$/)) {
+            total += getNumberValue(mainCategory.textContent);
+        }
     })
 
-    console.debug(`[YNAB Percentage web extension] Total budgeted for this month : ${total}`);
+    log.debug(`Total budgeted for this month : ${total}`);
 
     categories.forEach(mainCategory => {
-        console.log(mainCategory);
-        const value = getNumberValue(mainCategory.textContent, numberFormat);
-        const percentage = Math.round((((value / total) * 100) + Number.EPSILON) * 100) / 100;
-        console.log(`${mainCategory.innerHTML} (${percentage}%)`);
-        mainCategory.innerHTML = `${mainCategory.innerHTML} (${percentage}%)`;
+        if (!mainCategory.textContent.match(/^.*%\).*$/)) {
+            const value = getNumberValue(mainCategory.textContent);
+            const percentage = Math.round((((value / total) * 100) + Number.EPSILON) * 100) / 100;
+
+            const parser = new DOMParser();
+            const parsed = parser.parseFromString(`${mainCategory.innerHTML} (${percentage}%)`, `text/html`);
+            const parts = parsed.getElementsByTagName(`body`);
+            mainCategory.innerHTML = ``;
+            for (const part of parts) {
+                mainCategory.appendChild(part)
+            }
+        }
     })
 }
 
-console.debug(`${prefix} Loaded`);
-const initialCategories = document.querySelectorAll(mainCategorySelector);
+const main = () => {
+    log.debug(`Loaded`);
+    const initialCategories = document.querySelectorAll(mainCategorySelector);
 
-const observer = new MutationObserver(function (mutations, me) {
-    const categories = document.querySelectorAll(mainCategorySelector);
-    if (categories && categories.length > 0) {
-        updateCategoriesPercentage(categories);
-        me.disconnect();
-        return;
-    }
-});
+    const observer = new MutationObserver(function (mutations, me) {
+        const categories = document.querySelectorAll(mainCategorySelector);
+        if (categories && categories.length > 0) {
+            updateCategoriesPercentage(categories);
+            return;
+        }
+    });
 
-if (initialCategories && initialCategories.length > 0) {
-    console.debug(`${prefix} Run from initial categories`);
-    updateCategoriesPercentage(initialCategories);
-} else {
-    console.debug(`${prefix} Run from mutation observer`);
     observer.observe(document, {
         childList: true,
         subtree: true
     });
+
+    if (initialCategories && initialCategories.length > 0) {
+        log.debug(`Loaded : Run from initial categories`);
+        updateCategoriesPercentage(initialCategories);
+    }
 }
 
-
+main();
